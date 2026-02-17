50df36e543f80b715107c7cf6d4dd12678db2ff4
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-17 11:35:48 +0600

    getelement by classname and id

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-const listCollection = document.getElementsByTagName("li");
+// getElementsByTagName
+
+/* const listCollection = document.getElementsByTagName("li");
 console.log(listCollection);
 for(const li of listCollection){
     console.log(li.innerText);
+} */
+
+// getElementsByClassName VS getElementById
+
+const foodTile = document.getElementById("food_title")
+console.log(foodTile.innerText);
+
+const getByClass = document.getElementsByClassName("foreign")
+console.log(getByClass);
+for(const food of getByClass){
+    console.log(food.innerText);
+    
 }
 
